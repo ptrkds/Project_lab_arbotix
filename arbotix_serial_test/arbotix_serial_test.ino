8b297daa75dad1d561b558b15d41364572841e5e
Patrik Dosa <dosa.trik@gmail.com>
2018-09-21 15:25:39 +0200

    Default test + Serial test

---
+++ arbotix_serial_test/arbotix_serial_test.ino
@@ -0,0 +1,71 @@
+
+#include <Commander.h>
+#include <ax12.h>
+#include <BioloidController.h>
+
+void setup() {
+
+  //open serial port
+   Serial.begin(9600);
+   delay (500);   
+   Serial.println("###########################");    
+   Serial.println("Serial Communication Established.");  
+  
+  // set everything to center
+    Serial.println("###########################");
+    Serial.println("Set all servo motor to center (512).");
+    for (int i = 1; i <= 5; i++){
+      SetPosition(i, 512);
+      delay(100);
+    } 
+
+    Serial.println("###########################");
+    Serial.println("Relax all servo motor.");  
+    for (int i = 1; i <= 5; i++){
+      Relax(i); 
+    }
+    
+}
+
+void loop() {
+  
+  // read the servo motor number
+    Serial.println("###########################");
+    Serial.println("Which servo motor?"); 
+    
+    while(!Serial.available()){} // wait til the serial input
+    char servoInByte = Serial.read();
+    int servoNumber = servoInByte - '0';
+    
+    unsigned int positionValue=0;  // Max value is 65535
+    char incomingByte;
+    
+  // read position value
+    Serial.println("###########################");
+    Serial.println("Which position?");
+    
+  
+    while(!Serial.available()){}
+    delay(100);
+    if (Serial.available() > 0) {   // something came across serial
+      positionValue = 0;         // throw away previous integerValue
+      while(Serial.available() > 0) {
+        incomingByte = Serial.read();
+        
+        // Serial.println(incomingByte);  // just for test
+    
+        positionValue *= 10;  // shift left 1 decimal place
+        
+        // convert ASCII to integer, add, and shift left 1 decimal place
+        positionValue = ( (incomingByte - '0') + positionValue);
+      }
+      
+      SetPosition(servoNumber,positionValue);
+      Relax(servoNumber); 
+      
+      Serial.print("Servo");
+      Serial.print(servoNumber);
+      Serial.print(" in Position: ");
+      Serial.println(positionValue);
+    }
+}
---
+++ servo_zero_test/servo_zero_test.ino
@@ -0,0 +1,50 @@
+/***************************
+ * AXSimpleTest
+ * This sketch sends positional commands to the AX servo 
+ * attached to it - the servo must set to ID # 1
+ * The sketch will send a value, i, to the servo.
+ * 'For' loops are used to increment and decrement the value of 'i'
+ ***************************/
+
+//import ax12 library to send DYNAMIXEL commands
+#include <ax12.h>
+
+void setup()
+{
+    SetPosition(1,512); //set the position of servo # 1 to '0'
+    delay(100);//wait for servo to move
+    SetPosition(2,512); //set the position of servo # 1 to '0'
+    delay(100);//wait for servo to move
+    SetPosition(3,512); //set the position of servo # 1 to '0'
+    delay(100);//wait for servo to move
+    SetPosition(4,512); //set the position of servo # 1 to '0'
+    delay(100);//wait for servo to move
+    SetPosition(5,512); //set the position of servo # 1 to '0'
+    delay(100);//wait for servo to move
+    
+    Relax(1);
+    Relax(2);
+    Relax(3);
+    Relax(4);
+    Relax(5);
+
+}
+
+void loop()
+{
+  //increment from 0 to 1023
+  //for(int i=0;i<1024;i++)
+  //{
+  //  SetPosition(1,i); //set the position of servo #1 to the current value of 'i'
+  //  delay(10);//wait for servo to move
+  //}
+  //decrement from 1024 to 0
+  //for(int i=1024;i>0;i--)
+  //{
+  //  SetPosition(1,i);//set the position of servo #1 to the current value of 'i'
+  //  delay(10);//wait for servo to move
+  //}
+}
+
+
+
